36e5394cd8d04e0fa41cc99a163e9e9730bee88c
DanielGagliardi <33811407+DanielGagliardi@users.noreply.github.com>
2020-01-21 19:32:38 +0000

    index gen done

--- sketch.js
+++ sketch.js
@@ -10,22 +10,43 @@ class Complex {
 
 function Complex_Add(z, w) {
   //adds 2 complex numbers together
-  result = new Complex(z.re + w.re, z.im + w.im);
+  var result = new Complex(z.re + w.re, z.im + w.im);
   return result
 }
 
 function Complex_Multiply(z, w) {
   //adds 2 complex numbers together
-  result = new Complex(z.re * w.re - z.im * w.im, z.re * w.im + z.im * w.re);
+  var result = new Complex(z.re * w.re - z.im * w.im, z.re * w.im + z.im * w.re);
   return result
 }
-
 //testing complex functions
 /*
 var z = new Complex(1,1);
 var w = new Complex(2,2);
 console.log(Complex_Multiply(z,w).re, Complex_Multiply(z,w).im);
 */
+
+function Index_Gen(numberOfTerms) {
+	if (numberOfTerms === 0) {
+		return [];
+  } else if (numberOfTerms === 1) {
+		return [0];
+  } else {
+    var odd = numberOfTerms % 2;
+    indexes = [0];
+    for (index = 1; index <= numberOfTerms / 2  ; index++) {
+      indexes.push(index);
+      indexes.push(-1*index);
+    }
+    if (odd == 0) {
+      indexes.pop();
+    }
+    return indexes;
+  }
+}
+
+console.log(Index_Gen(3));
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
