2e623866a40b8b7ce82cab05a0f13eae2f28be21
DanielGagliardi <33811407+DanielGagliardi@users.noreply.github.com>
2020-01-21 19:16:22 +0000

    Added Complex arithmetic

--- sketch.js
+++ sketch.js
@@ -1,21 +1,45 @@
 window.onload = function() {
 }
 
+class Complex {
+  constructor(realPart, imaginaryPart) {
+      this.re = realPart;
+      this.im = imaginaryPart;
+  }
+}
+
+function Complex_Add(z, w) {
+  //adds 2 complex numbers together
+  result = new Complex(z.re + w.re, z.im + w.im);
+  return result
+}
+
+function Complex_Multiply(z, w) {
+  //adds 2 complex numbers together
+  result = new Complex(z.re * w.re - z.im * w.im, z.re * w.im + z.im * w.re);
+  return result
+}
+
+//testing complex functions
+/*
+var z = new Complex(1,1);
+var w = new Complex(2,2);
+console.log(Complex_Multiply(z,w).re, Complex_Multiply(z,w).im);
+*/
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background("black");
 
   beginShape();
-
   noFill();
   stroke(255);
   strokeWeight(3);
-
   for (i=0; i<points.length; i++) {
-    vertex(points[i][0], points[i][1]);
-    console.log(points[i][0], points[i][1]);
+    var xOffset = 600;
+    var yOffset = 200; 
+    vertex(points[i][0] + xOffset, points[i][1] + yOffset);
   }
-
   endShape(CLOSE);
 }
 
