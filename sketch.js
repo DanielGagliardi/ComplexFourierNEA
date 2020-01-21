504e599cd54af55c6ddeedf0ba636a0b98493da1
DanielGagliardi <33811407+DanielGagliardi@users.noreply.github.com>
2020-01-21 17:51:29 +0000

    undo

--- sketch.js
+++ sketch.js
@@ -1,10 +1,22 @@
 window.onload = function() {
-  console.log(points);
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(0);
+  background("black");
+
+  beginShape();
+
+  noFill();
+  stroke(255);
+  strokeWeight(3);
+
+  for (i=0; i<points.length; i++) {
+    vertex(points[i][0], points[i][1]);
+    console.log(points[i][0], points[i][1]);
+  }
+
+  endShape(CLOSE);
 }
 
 function draw() {
